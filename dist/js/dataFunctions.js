4b4c356dd0555cead9203d8971ce214155a4eef1
Ray Cho <razor0424@gmail.com>
2023-12-26 16:24:28 +0800

    added serverless functions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -16,7 +16,7 @@ export const cleanText = (rawText) => {
 };
 
 export const getCoordsFromAPI = async (entryText, units) => {
-    const regex = /^\d+$/g;
+    /* const regex = /^\d+$/g;
     const flag = regex.test(entryText) ? "zip" : "q";
     const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API}`;
     const encodedUrl = encodeURI(url);
@@ -27,17 +27,49 @@ export const getCoordsFromAPI = async (entryText, units) => {
         return jsonData;
     } catch (err) {
         console.error(err);
+    } */
+
+    const urlDataObj = {
+        text: entryText,
+        units: units,
+    };
+
+    try {
+        const dataStream = await fetch("./.netlify/functions/get_coords", {
+            method: "POST",
+            body: JSON.stringify(urlDataObj),
+        });
+        const jsonData = await dataStream.json();
+        return jsonData;
+    } catch (err) {
+        console.error(err);
     }
 };
 
 export const getWeatherFromCoords = async (locationObj) => {
-    const lat = locationObj.getLat();
+    /* const lat = locationObj.getLat();
     const lon = locationObj.getLon();
     const units = locationObj.getUnit();
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API}`;
-
+    
     try {
         const weatherStream = await fetch(url);
+        const weatherJson = await weatherStream.json();
+        return weatherJson;
+    } catch (err) {
+        console.error(err);
+    } */
+
+    const urlDataObj = {
+        lat: locationObj.getLat(),
+        lon: locationObj.getLon(),
+        units: locationObj.getUnit(),
+    };
+    try {
+        const weatherStream = await fetch("./.netlify/functions/get_weather", {
+            method: "POST",
+            body: JSON.stringify(urlDataObj),
+        });
         const weatherJson = await weatherStream.json();
         return weatherJson;
     } catch (err) {
